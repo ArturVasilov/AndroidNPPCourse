d5fd2c210cc648fa75765ac9de892283000dd5b3
Artur Vasilov <artur_8005@mail.ru>
2015-04-02 22:55:27 +0300

    add server side code

---
+++ CodeSamples, Part 2, Client-server applications/ServerSide/public_html/api/DatabaseConstants.php
@@ -0,0 +1,18 @@
+<?php
+
+class DatabaseConstants
+{
+    public static $DATABASE_NAME = "android_blog";
+
+    public static $RESPONSE = "response";
+
+    public static $ERROR_NO_ERRORS = 0;
+    public static $ERROR_PARAMS_TOKEN = "token_error";
+    public static $ERROR_SAME_LOGIN = "login_error";
+
+    public static $ERROR_RECORD_NOT_FOUND = 404;
+
+    public static $ERROR_ENGINE_PARAMS = 100;
+
+    public static $ERROR_ZIP_ARCHIVE = 1001;
+}
---
+++ CodeSamples, Part 2, Client-server applications/ServerSide/public_html/api/define.php
@@ -0,0 +1,7 @@
+<?php
+//This file is not real; it contains important information, so I've changed it
+define('DB_HOST', 'your database host (e.g. localhost)');
+define('DB_USER', 'database username');
+define('DB_PASS', 'database password');
+define('DB_NAME', 'database name');
+define('API_PASS', 'api password');
